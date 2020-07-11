700bb041e52137781001fb9eab299c2b12ae07de
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-07-10 21:02:44 -0500

    committed

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Route, Link} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
-
-// function ML(props) {
-//   return <MovieList props={}/>
-// }
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -27,36 +23,14 @@ const App = () => {
     getMovies();
   }, []);
   console.log(movieList);
-  // console.log(savedList);
+
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
-  // let movieMap = movieList.map(x => {
-  //   return `/movies/${x.id}`;
-  // })
-  // console.log(movieMap);
-  // const fetchMovies = id => Promise.resolve(
-  //   movieList.find(movie => movie.id == id)
-  // );
-
-  // const Movie1 = props => {
-  //   const {id} = props.match.params;
-  //   const [movie, setMovie] = useState(null);
-
-  //   useEffect(() => {
-  //     fetchMovies(id).then(movie => setMovie(movie));
-  //   }, [id])
-
-  //   if (!movie) return <div>Fetching movie...</div>
-  //   return( <div>The movie is {movie.name}</div> );
-  // }
-  // console.log(Movie1);
-
   return (
     <div>
       <SavedList list={savedList} />
-      {/* <Link to={ML}>Movies</Link> */}
 
       <Route exact path='/'>
         <MovieList movies={movieList}/>
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -4,13 +4,9 @@ import {useParams} from 'react-router-dom'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
   const {id} = useParams();
-  useEffect(() => {
-    // const id = useParams();
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
 
+  useEffect(() => {
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
@@ -19,7 +15,6 @@ const Movie = (props) => {
         .catch(error => {
           console.error(error);
         });
-
   },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
